5b6f01a5f3a7e9e59330a667ba909dc5b64a0b3b
VeselaP02 <vpetkova0204@abv.bg>
2025-03-09 13:28:04 +0200

    Add methods in AuthorRepository and BookRepository

--- src/main/java/book_library/repositories/AuthorRepository.java
+++ src/main/java/book_library/repositories/AuthorRepository.java
@@ -2,12 +2,23 @@ package book_library.repositories;
 
 import book_library.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long> {
 
-    Author findAuthorById(Long id);
+    List<Author> findByFirstNameIgnoreCase(String firstName);
+
+    List<Author> findByLastNameIgnoreCase(String lastName);
+
+    List<Author> findByFirstNameAndLastNameIgnoreCase(String firstName,String lastName);
+
+    @Query("SELECT DISTINCT a FROM Author a JOIN a.books b WHERE b.releaseDate > :date")
+    List<Author> findByReleaseDateAfter(@Param("date") LocalDate date);
+
 }
--- src/main/java/book_library/repositories/BookRepository.java
+++ src/main/java/book_library/repositories/BookRepository.java
@@ -2,6 +2,7 @@ package book_library.repositories;
 
 import book_library.entities.Author;
 import book_library.entities.Book;
+import book_library.enums.BookStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,7 +16,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByTitle(String title);
 
-    @Query("SELECT b FROM Book b " +
-            " WHERE b.author.firstName = :firstName AND b.author.lastName = :lastName")
-    List<Book> findByAuthor_FirstNameAndAuthor_LastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
+    List<Book> findByAuthor_FirstNameAndAuthor_LastName(String firstName, String lastName);
+
+    List<Book> findByLibraryBranch_Name(String branchName);
+
+    List<Book> findByBookStatus(BookStatus bookStatus);
 }
