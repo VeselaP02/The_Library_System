456007aabacbe1d2bcf53eae57b52f8065347e5b
VeselaP02 <vpetkova0204@abv.bg>
2025-03-10 23:55:04 +0200

    Add methods in BorrowRecordRepository

--- src/main/java/book_library/repositories/BorrowRecordRepository.java
+++ src/main/java/book_library/repositories/BorrowRecordRepository.java
@@ -7,8 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BorrowRecordRepository extends JpaRepository<BorrowRecord,Long> {
 
     BorrowRecord findByUserAndBookAndBorrowDate(User user, Book book, LocalDate borrowDate);
+
+    List<BorrowRecord> findAllByUser(User user);
+
+    List<BorrowRecord> findAllByReturnDateIsNull();
+
+    List<BorrowRecord> findAllByLibraryBranch_Name(String branchName);
 }
