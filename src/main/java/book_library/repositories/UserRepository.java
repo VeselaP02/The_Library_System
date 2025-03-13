b6ab474648aec8df7b14160f59e3ef39962485f4
VeselaP02 <vpetkova0204@abv.bg>
2025-03-13 23:46:29 +0200

    Add methods in UserRepository

--- src/main/java/book_library/repositories/UserRepository.java
+++ src/main/java/book_library/repositories/UserRepository.java
@@ -2,10 +2,21 @@ package book_library.repositories;
 
 import book_library.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByUsername(String username);
+
+    List<User> findByRegistrationDateAfter(LocalDate date);
+
+    @Query("SELECT u FROM User u WHERE u.libraryBranch.id = :branchId")
+    List<User> findByLibraryBranch(@Param("branchId") Long branchId);
+
 }
